144d138ef5bdd3273436bc0107e4eef1f8224147
szolim <miloszrekin123@gmail.com>
2022-02-19 14:12:07 +0100

    documenting code and slightly optimising; WIP

--- player_lib.py
+++ player_lib.py
@@ -1,15 +1,19 @@
 import pygame
 
-class Player():
+
+class Player:
     def __init__(self, pos, speed=2) -> None:
-        #Pygame surf and rect object setup
-        self.sprite = pygame.image.load("src/images/player.png")      
+        """Create an instance of Player object, consisting of
+        pygame.Surface, pygame.Rects and all the variables
+        used by physics engine"""
+        # Pygame surf and rect object setup
+        self.sprite = pygame.image.load("src/images/player.png")
         self.sprite.set_colorkey((0, 0, 0))
         self.rect = self.sprite.get_rect()
         self.rect.x, self.rect.y = pos[0], pos[1]
-        #Inputs
-        self.inputs = {"jump":0,"squat":0,"left":0,"right":0}
-        #Physics and render engine variables
+        # Inputs
+        self.inputs = {"jump": 0, "squat": 0, "left": 0, "right": 0}
+        # Physics and render engine variables
         self.speed = speed
         self.x_movement = 0
         self.y_movement = 0
@@ -17,8 +21,9 @@ class Player():
         self.momentum = 0
         self.g_force = 0.14
 
-
     def eval_inputs(self, event):
+        """For every pressed or released key, check if player has an action
+        variable assigned to the key and change it accordingly"""
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
                 self.inputs["jump"] = 1
@@ -38,42 +43,52 @@ class Player():
             if event.key == pygame.K_a:
                 self.inputs["left"] = 0
 
-
     def collision_test(self, tiles):
+        """For every tile in the list of rects passed as an argument,
+        run a pygame func checking if it collides with player rect
+        and if it does, add it to the list and return the list"""
         collisions = []
         for tile in tiles:
             if self.rect.colliderect(tile):
                 collisions.append(tile)
         return collisions
 
-
     def eval_movement(self):
+        """Check the inputs related to the player's movement
+        calculate the  horizontal movement using them, calculate vertical movement
+        using physics engine (momentum, g_force); check if player can jump and tries to jump"""
         x_input = self.inputs["right"] - self.inputs["left"]
         y_input = self.inputs["squat"] - self.inputs["jump"]
         self.x_movement = x_input * self.speed
-        self.momentum = min(3, self.momentum+self.g_force)
+        self.momentum = min(3, self.momentum + self.g_force)
         self.y_movement = self.momentum
-        just_jumped = False
+
         if self.jump_ability > 0 and y_input == -1:
-            just_jumped = True
             self.momentum = -3
             self.jump_ability -= 1
-        return just_jumped
-
 
     def clamp_to_level_edge(self, level_size):
+        """In case player gets to the edge of the world/level, clamp his position
+        to edges of the level"""
         self.rect.left = max(self.rect.left, 0)
         self.rect.right = min(self.rect.right, level_size[0])
         self.rect.top = max(self.rect.top, 0)
         self.rect.bottom = min(self.rect.bottom, level_size[1])
 
-
     def update(self, tile_rects, level_size) -> None:
-        just_jumped = self.eval_movement()
-        #moving and colliding
+        """Updates state of the player. Calls eval_movement to update how the
+        player should move according to inputs and physics. Adds the values calculated
+        in eval_movement one at a time and applies collisions. First checks movement and
+        collisions in one axis, then in the other."""
+        self.eval_movement()  # calculate movement
+
+        # reset collision that player has with environment
         collision_types = {"top": False, "bottom": False, "right": False, "left": False}
-        
-        self.rect.x += self.x_movement       #horizontal
+
+        # Calculates player's x coordinate and overwrites it in case the new value would
+        # push a player into a physical tile
+        self.rect.x += self.x_movement
+        # Runs a collision test; calls pygame collision func over player and every tile
         for tile in self.collision_test(tile_rects):
             if self.x_movement > 0:
                 self.rect.right = tile.left
@@ -81,13 +96,16 @@ class Player():
             if self.x_movement < 0:
                 self.rect.left = tile.right
                 collision_types["left"] = True
-       
-        self.rect.y += self.y_movement      #vertical
-        self.jump_ability = 0
+
+        # Calculates player's y coordinate and overwrites it in case the new value would
+        # push a player into a physical tile
+        self.rect.y += self.y_movement
+        # self.jump_ability = 0
+        # collision_test ran again to check for collisions after establishing x value, to calculate y value
         for tile in self.collision_test(tile_rects):
             if self.y_movement > 0:
                 self.rect.bottom = tile.top
-                if just_jumped == False:
+                if self.jump_ability == 0:
                     self.momentum = 0
                     self.jump_ability = 1
                 collision_types["bottom"] = True
@@ -96,5 +114,5 @@ class Player():
                 self.momentum = 0
                 collision_types["top"] = True
 
-        #Screen edge collisions
+        # Screen edge collisions
         self.clamp_to_level_edge(level_size)
--- setup_lib.py
+++ setup_lib.py
@@ -1,23 +1,28 @@
 import os, pygame, json
 
+
 def set_settings(json_file):
+    """Save all the general settings to a json file"""
     settings_dict = {}
-    settings_dict["src_path"] =  os.path.dirname(os.path.realpath(__file__))
-    settings_dict["window_size"] = (1600,900)
-    with open(json_file,"r+") as f:
+    settings_dict["src_path"] = os.path.dirname(os.path.realpath(__file__))
+    settings_dict["window_size"] = (1600, 900)
+    with open(json_file, "r+") as f:
         json.dump(settings_dict, f, indent=4)
 
 
 def get_settings(json_file):
+    """load all the needed settings from a json file as a dictionary"""
     settings_dict = {}
     with open(json_file, "r") as f:
         settings = json.load(f)
         for k, v in settings.items():
             settings_dict[k] = v
     return settings_dict
-            
+
 
 def load_images(src_dir):
+    """Create a dictionary of assets(images) where key is name of
+    .png file wihtout extension, and value is a pygame.surface loaded from that file"""
     directory = os.path.join(src_dir, "images")
     img_files_list = []
     image_dict = {}
